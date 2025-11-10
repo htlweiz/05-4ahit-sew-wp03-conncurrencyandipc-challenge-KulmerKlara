89a508bca706135de1fe56a653d2b1c72ba2e717
KulmerKlara <klara.kulmer@outlook.com>
2025-11-10 08:46:21 +0100

    programm bankacount

--- A2_RaceConditionBank/BankAccount.cs
+++ A2_RaceConditionBank/BankAccount.cs
@@ -2,28 +2,31 @@ using System;
 using System.Threading;
 
 namespace A2_RaceConditionBank;
-public class BankAccount
-{
-    private int balance;
-   
-    
-    public BankAccount(int initial) 
-    { 
-        balance = initial; 
-    }
-    
-    public void Deposit(int amount) 
-    { 
-       
-    }
-    
-    public void Withdraw(int amount) 
-    { 
-        
-    }
-    
-    public int GetBalance() 
+
+    public class BankAccount
     {
-        return balance;
-    }
+        private int balance;
+
+        public BankAccount(int initial)
+        {
+            balance = initial;
+        }
+
+        public void Deposit(int amount)
+        {
+            int depositAmount = balance + amount;
+            balance = depositAmount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            int withdrawAmount = balance - amount;
+            balance = withdrawAmount;
+        }
+
+        public int GetBalance()
+        {
+            return balance;
+        }
 }
+
--- A2_RaceConditionBank/Program.cs
+++ A2_RaceConditionBank/Program.cs
@@ -2,23 +2,37 @@ using System;
 using System.Threading;
 
 namespace A2_RaceConditionBank;
-
 class Program
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Übung 2: Race Condition – Bankkonto");
-        Console.WriteLine("==========================================\n");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Übung 2: Race Condition – Bankkonto");
+            Console.WriteLine("==========================================\n");
+            
+            // Startwert
+            BankAccount account = new BankAccount(1000);
+            Console.WriteLine($"Startkontostand: {account.GetBalance()} EUR\n");
+
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => PerformBankOperations(account));
+                threads[i].Start();
+            }
+
+            foreach (Thread t in threads)
+                t.Join();
+
+            Console.WriteLine($"\nEndkontostand: {account.GetBalance()} EUR");
+        }
         
-        // Bankkonto mit Startwert 1000 EUR erstellen
-        BankAccount account = new BankAccount(1000);
-        Console.WriteLine($"Startkontostand: {account.GetBalance()} EUR\n");
-        
+        private static void PerformBankOperations(BankAccount account)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                account.Withdraw(100);  
+                account.Deposit(150);   
+            }
+        }
     }
-    
-    private static void PerformBankOperations(BankAccount account)
-    {
-        
-    }
-}
 
